e3a6c536b89c034249e4f5fe67403c2592d29597
Anneline <anneline.guttormsen@hotmail.no>
2020-03-18 14:05:14 +0100

    Finished

--- src/components/NewsArticleList.js
+++ src/components/NewsArticleList.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function NewsArticleList() {
+  const [state, setState] = useState({
+    articles: []
+  });
+
+  useEffect(
+    function() {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(articles => setState({
+          articles: articles
+        }))
+    }, []
+  );
 
   return (
     <ul>
+      {
+        state.articles
+          .map(article => <li key={article.id}>{article.title}</li>)
+      }
     </ul>
   );
 }
